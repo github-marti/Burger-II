1e6b6a03afca88427171202019f50dc850880bff
Marti <52050778+github-marti@users.noreply.github.com>
2019-10-26 14:09:49 -0700

    added missing sequelize sync

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+var db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -23,8 +25,8 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 app.use(express.static('public'));
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
