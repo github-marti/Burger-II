460be05923f83c59c90f5fea7d4103eded4eaa09
Marti <52050778+github-marti@users.noreply.github.com>
2019-10-26 14:10:45 -0700

    added customer column to index.html

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -25,15 +25,15 @@ $(function() {
       event.preventDefault();
   
       var newBurger = {
-        burger_name: $("#new-burger").val().trim(),
+        burger: {burger_name: $("#new-burger").val().trim()},
+        customer: {customer_name: $("#new-customer").val().trim()}
       };
   
       // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
-      }).then(
-        function() {
+      }).then(function() {
           console.log("created new burger");
           // Reload the page to get the updated list
           location.reload();
