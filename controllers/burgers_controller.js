62dc75c3644d2a8e16e99621c900a534cb247466
Marti <52050778+github-marti@users.noreply.github.com>
2019-10-26 15:36:45 -0700

    added table join into burgers controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,58 +1,60 @@
-const express = require("express");
-const router = express.Router();
 const db = require("../models");
 
+module.exports = function(app) {
+    app.get("/", function(req, res) {
 
-router.get("/", function(req, res) {
-    db.Burger.findAll()
-    .then(results => {
-        let hbsObject = {
-            burgers: results
-        };
-        console.log('hbsObject', hbsObject);
-        res.render("index", hbsObject);
+        db.Burger.findAll({
+            include: [
+              {
+                model: db.Customer
+              }
+            ]
+          })
+        .then(results => {
+            let hbsObject = {
+                burgers: results
+            };
+            console.log("burger get results", results);
+            console.log('hbsObject', hbsObject);
+            res.render("index", hbsObject);
+        });
     });
-});
 
-router.post("/api/burgers", function(req, res) {
-    db.Burger.create(req.body.burger)
-    .then(
-        db.Customer.create(req.body.customer)
-        .then(
-        results => {
-        res.json({ id: results.insertId });
-        })
-    );
-});
+    app.post("/api/burgers", function(req, res) {
+        db.Burger.create(req.body)
+        .then(results => {
+            res.json({ id: results.insertId });
+            }
+        );
+    });
 
-router.put("/api/burgers/:id", function(req, res) {
-    db.Burger.update(
-        req.body,
-        {
+    app.put("/api/burgers/:id", function(req, res) {
+        db.Burger.update(
+            req.body,
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        )
+        .then(results => {
+            if (results.changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+    app.delete("/api/burgers/:id", function(req, res) {
+        db.Burger.destroy({
             where: {
                 id: req.params.id
             }
-        }
-    )
-    .then(results => {
-        if (results.changedRows === 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-    db.Burger.destroy({
-        where: {
-            id: req.params.id
-        }
+        })
+        .then(result => {
+            console.log(result);
+            res.end();
+        })
     })
-    .then(result => {
-        console.log(result);
-        res.end();
-    })
-})
-
-module.exports = router;
+};
