e5544e1923d206f68a48d2441f98e00ded8355ef
Marti <52050778+github-marti@users.noreply.github.com>
2019-10-26 15:39:02 -0700

    udpated burgers.js to add createBurger and createCustomer functions that would act asynchronously to receive customer id first before creating burger. Also included code into handlebars to accept customer name in ordered/devoured columns

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,54 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+$(document).ready(function() {
+
+    //functions
+
+    function createBurger(newBurger) {
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+        }
+      );
+    };
+
+    function createCustomer(newCustomer) {
+      return new Promise ((resolve, reject) => {
+        $.ajax("/api/customers", {
+          type: "POST",
+          data: newCustomer
+        }).then(results => {
+          resolve(results);
+        }
+        );
+      })
+    };
+
+    // event handlers
+    
+    // creates the burger and adds new rows to burger table and customer table
+    $(".burger-form").on("submit", async function(event) {
+      event.preventDefault();
+
+      let newCustomer = {
+        customer_name: $("#new-customer").val().trim()
+      }
+
+      let customer = await createCustomer(newCustomer)
+  
+      let newBurger = {
+        burger_name: $("#new-burger").val().trim(),
+        CustomerId: customer.id
+      };
+
+      createBurger(newBurger);
+
+      
+    });
+
+    // changes devoured status on button click and reloads page
     $(".devour-btn").on("click", function(event) {
       let id = $(this).data("id");
   
@@ -7,40 +56,18 @@ $(function() {
         devoured: true
       };
   
-      // Send the PUT request.
       $.ajax(`/api/burgers/${id}`, {
         type: "PUT",
         data: newStatus
       }).then(
         function() {
           console.log("burger has been devoured");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".burger-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        burger: {burger_name: $("#new-burger").val().trim()},
-        customer: {customer_name: $("#new-customer").val().trim()}
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
           location.reload();
         }
       );
     });
 
+    // gets rid of burger/customer info on button click
     $(".burger-delete").on("click", function() {
         var id = $(this).data('id');
         $.ajax(`/api/burgers/${id}`, {
